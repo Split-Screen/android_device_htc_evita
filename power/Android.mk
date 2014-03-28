f14986e92b50b5e130d9bf367bcc1542e0b4c227
Steve Kondik <shade@chemlab.org>
2014-03-27 22:00:00 -0700

    common-qcom: Add PowerHAL
     * New PowerHAL for QCOM devices based on upstream.
     * Requires libqc-opt and mpdecision
     * Includes support for new audio hints
     * Includes hook for external set_interactive bits
    
    Change-Id: Ib5c956fedd1a4aee591b409b1524e95624324480

---
+++ power/Android.mk
@@ -0,0 +1,31 @@
+LOCAL_PATH := $(call my-dir)
+
+ifneq ($(TARGET_PROVIDES_POWERHAL),true)
+ifneq ($(TARGET_USES_CM_POWERHAL),true)
+
+ifneq (,$(filter true,$(BOARD_USES_QCOM_HARDWARE) $(WITH_QC_PERF)))
+
+# HAL module implemenation stored in
+# hw/<POWERS_HARDWARE_MODULE_ID>.<ro.hardware>.so
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)/hw
+LOCAL_SHARED_LIBRARIES := liblog libcutils libdl
+LOCAL_SRC_FILES := power.c metadata-parser.c utils.c list.c hint-data.c power-8960.c
+
+ifeq ($(TARGET_USES_INTERACTION_BOOST),true)
+    LOCAL_CFLAGS += -DINTERACTION_BOOST
+endif
+
+ifneq ($(TARGET_POWERHAL_SET_INTERACTIVE_EXT),)
+LOCAL_CFLAGS += -DSET_INTERACTIVE_EXT
+LOCAL_SRC_FILES += ../../../../$(TARGET_POWERHAL_SET_INTERACTIVE_EXT)
+endif
+
+LOCAL_MODULE := power.$(TARGET_BOARD_PLATFORM)
+LOCAL_MODULE_TAGS := optional
+include $(BUILD_SHARED_LIBRARY)
+
+endif # BOARD_USES_QCOM_HARDWARE || WITH_QC_PERF
+endif # !TARGET_USES_CM_POWERHAL
+endif # !TARGET_PROVIDES_POWERHAL
