c9787cdd0d0d851c33097e461f5e04856485e0bd
Steve Kondik <shade@chemlab.org>
2014-05-17 03:37:53 -0700

    power: Add support for CPU boost hint
    Change-Id: I07c3e8daf8a5f3b568e27334f26d4ba8f4cf40c4

--- power/Android.mk
+++ power/Android.mk
@@ -15,8 +15,8 @@ LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)/hw
 LOCAL_SHARED_LIBRARIES := liblog libcutils libdl
 LOCAL_SRC_FILES := power.c metadata-parser.c utils.c list.c hint-data.c power-8960.c
 
-ifeq ($(TARGET_USES_INTERACTION_BOOST),true)
-    LOCAL_CFLAGS += -DINTERACTION_BOOST
+ifeq ($(TARGET_USES_CPU_BOOST_HINT),true)
+    LOCAL_CFLAGS += -DBOOST_HINT
 endif
 
 ifneq ($(TARGET_POWERHAL_SET_INTERACTIVE_EXT),)
