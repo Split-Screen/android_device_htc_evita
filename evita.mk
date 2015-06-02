6a8bc4a59075532d283935613d1992f407a610c8
Nick Kralevich <nnk@google.com>
2015-06-02 16:00:21 -0700

    remove persist.sys.usb.config override
    The variable persist.sys.usb.config is no longer honored by the framework,
    and it's presence causes unnecessary toggling of the USB driver, which
    disonnects ADB and makes the device's connection unstable.
    
    Delete it.
    
    Bug: 21404762
    Bug: 18905620
    Change-Id: Ie456319637de15e27ec74cad4884bb7724110896

--- evita.mk
+++ evita.mk
@@ -91,7 +91,6 @@ PRODUCT_PACKAGES += \
 ADDITIONAL_DEFAULT_PROPERTIES += \
     persist.service.adb.enable=1 \
     persist.service.debuggable=1 \
-    persist.sys.usb.config=mtp \
     ro.adb.secure=0
 
 # Audio
