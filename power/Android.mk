514a2c68f35404a1442e4a6e0aa3f2059d5478bf
José Adolfo Galdámez <josegalre@pac-rom.com>
2015-07-15 00:09:45 -0600

    power: only run with evita
    Signed-off-by: José Adolfo Galdámez <josegalre@pac-rom.com>

--- power/Android.mk
+++ power/Android.mk
@@ -1,32 +1,14 @@
 LOCAL_PATH := $(call my-dir)
 
-# Hey Mr. Make Author, DIAF PLX
-ifeq ($(TARGET_POWERHAL_VARIANT),qcom)
-USE_ME := true
-endif
+ifeq ($(TARGET_POWERHAL_VARIANT),evita)
 
-ifneq (,$(filter true,$(USE_ME) $(WITH_QC_PERF)))
-
-# HAL module implemenation stored in
-# hw/<POWERS_HARDWARE_MODULE_ID>.<ro.hardware>.so
 include $(CLEAR_VARS)
-
+LOCAL_MODULE               := power.msm8960
 LOCAL_MODULE_RELATIVE_PATH := hw
-LOCAL_PROPRIETARY_MODULE := true
-LOCAL_SHARED_LIBRARIES := liblog libcutils libdl
-LOCAL_SRC_FILES := power.c metadata-parser.c utils.c list.c hint-data.c power-8960.c
-
-ifneq ($(TARGET_POWERHAL_SET_INTERACTIVE_EXT),)
-LOCAL_CFLAGS += -DSET_INTERACTIVE_EXT
-LOCAL_SRC_FILES += ../../../../$(TARGET_POWERHAL_SET_INTERACTIVE_EXT)
-endif
-
-ifneq ($(CM_POWERHAL_EXTENSION),)
-LOCAL_MODULE := power.$(CM_POWERHAL_EXTENSION)
-else
-LOCAL_MODULE := power.$(TARGET_BOARD_PLATFORM)
-endif
-LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_TAGS          := optional
+LOCAL_PROPRIETARY_MODULE   := true
+LOCAL_SHARED_LIBRARIES     := liblog libcutils libdl
+LOCAL_SRC_FILES            := power.c metadata-parser.c utils.c list.c hint-data.c power-8960.c
 include $(BUILD_SHARED_LIBRARY)
 
-endif # TARGET_POWERHAL_VARIANT == qcom || WITH_QC_PERF
+endif
