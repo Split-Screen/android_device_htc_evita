f409272f6869734e44f1c1c353e11a971bb0d47d
Gabriele M <moto.falcon.git@gmail.com>
2015-12-05 06:55:29 +0100

    Custom PowerHAL implementation
    PowerHAL highlights:
     * The CPU governor is always interactive. Governor changes are not
       expected and permissions are set once on boot.
    
     * Powersave profile: the CPU frequency is limited 700MHz and the CPU
       is never boosted on user interaction. This will save power without
       making the phone unbearably slow.
    
     * Balanced profile: the CPU is boosted on user interaction.
       When the screen is turned off, the governor is tuned to lower the
       power consumption to save power in case of long lasting screen off
       activities such as music playback.
    
     * Performance profile: the CPU is constantly boosted.
    
     * Currently there are no restrictions on the number of active cores.
    
     * With this PowerHAL cpu-boost is not needed, so keep it disabled.
    
    Change-Id: I6bb01a14f0058c59986989568e7766f4203150cc

--- power/Android.mk
+++ power/Android.mk
@@ -1,14 +1,25 @@
-LOCAL_PATH := $(call my-dir)
+#
+# Copyright 2015 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
-ifeq ($(TARGET_POWERHAL_VARIANT),evita)
+LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE               := power.msm8960
 LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_MODULE_TAGS          := optional
-LOCAL_PROPRIETARY_MODULE   := true
-LOCAL_SHARED_LIBRARIES     := liblog libcutils libdl
-LOCAL_SRC_FILES            := power.c metadata-parser.c utils.c list.c hint-data.c power-8960.c
+LOCAL_SHARED_LIBRARIES     := liblog libcutils
+LOCAL_SRC_FILES            := power.c
 include $(BUILD_SHARED_LIBRARY)
-
-endif
